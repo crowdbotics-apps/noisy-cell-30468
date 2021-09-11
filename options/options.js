1c90ad05d62bd999305f2ee92c9699d367d834a9
Crowdbotics <team@crowdbotics.com>
2021-09-11 12:40:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "noisy_cell_30468Identifier",
+  url: "https://noisy_cell_30468Identifier.botics.co",
+  api: "https://noisy_cell_30468Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
